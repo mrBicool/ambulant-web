ef869908bf29212054308dbbf76bccd13559523d
David <gualvezdavid@gmail.com>
2019-07-13 15:35:26 +0800

    updates

--- public/js/config.js
+++ public/js/config.js
@@ -17,8 +17,8 @@ $(document).ready(function(){
 }); 
 
 //global variable for all page  
-var api = 'http://172.16.12.115:8007/api';
-var local_printer_api = "http://172.16.12.115:8009/api";
+var api = 'http://172.16.12.37:8005/api';
+var local_printer_api = "http://172.16.12.37:8007/api";
 var routes = {
     login:                      '/login',
     categories:                 '/outlet/category',
